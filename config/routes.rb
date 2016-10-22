834a6f76845b12e6015e370829b9a6e104e0dc3d
octothorpus <octothorpus@gmail.com>
2016-10-22 14:52:37 -0700

    add models

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+  #this is a comment
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
